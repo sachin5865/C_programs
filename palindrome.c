63747ffc1dda0701581ea3102ca73a5ead88af90
sachin kumar <67458131+sachin5865@users.noreply.github.com>
2023-09-02 23:36:46 +0530

    Add files via upload

---
+++ palindrome.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+// int main(){
+//     char s[100];
+//     int i, n,c=0;
+//     printf("enter the string:\n");
+//     gets(s);
+//     n = strlen(s);
+//     for(i=0; i=n/2; i++){
+//         if(s[i] == s[n-i-1]);
+//         c++;
+//         if(c==i)
+//         printf("palingrome");
+//         else
+//         printf("not");
+//     }
+//     return 0;
+// }
+
+
+
+// ---------------------------------------------------------------------------------
+
+// int main(){
+//     char s[100];
+//     int i ,ch;
+//     printf("enter the string:\n");
+//     gets(s);
+//     printf("enter which you want to capital:");
+//     scanf("%d",&i);
+//     ch = s[i-1];
+//     ch = ch-32;
+//     s[i-1]=ch;
+//     puts(s);  
+// }
+
+
+// -----------------------------------------------------------------------
+// reverse string
+
+
+// int main(){
+//     char ch [50];
+//     printf("enter the string:");
+//     gets(ch);
+//     printf("%s",strrev(ch));
+//     return 0;
+// }
+
+// --------------------------------------------------------------------
+
+
+// void main()  
+// {  
+//     char str[50], temp; 
+//     int i, left, right, len;  
+//     printf (" \n Enter a string to reverse order: ");  
+//     scanf( "%s", &str);  
+//     len = strlen(str); 
+//     left = 0; 
+//     right = len - 1; 
+//     for (i = left; i <right; i++)  
+//     {  
+//         temp = str[i];  
+//         str[i] = str[right];  
+//         str[right] = temp;  
+//         right--;  
+//     }  
+//     printf (" The reverse of the original string is: %s ",  str);  
+//     getch();  
+// }  
+
+
+// fing a substring from n to m in a STRING
+// find a num of operands of a 
+// check whether a give string is anagram or not
+// sum of odd digit and even digit
